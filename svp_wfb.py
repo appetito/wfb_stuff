381454b5cd5d09f5a8c104b392a0129aa330c440
Yuriy Homyakov <Iurii.Khomiakov@kronshtadt.ru>
2020-07-10 12:02:06 +0300

    air mavlink router

--- svp_wfb.py
+++ svp_wfb.py
@@ -101,22 +101,32 @@ class GCSLinkProtocol:
     def __init__(self, uplink):
         self.uplink = uplink
         self.bytes_cnt = 0
+        self.ra = None
 
     def connection_made(self, transport):
         self.transport = transport
 
     def datagram_received(self, data, addr):
+        self.ra = addr
         self.bytes_cnt += len(data)
         self.uplink.sendto(data, ('127.0.0.1', 5557))
 
     def error_received(self, exc):
         pass
 
+    def connection_lost(self, exc):
+        pass
+
+    def sendto(self, data, addr=None):
+        if self.ra:
+            self.transport.sendto(data, self.ra)
+
 
 
 class MavProxy:
 
-    def __init__(self, telem_chan):
+    def __init__(self, mode, telem_chan):
+        self.mode = mode
         self.telem_chan = telem_chan
         self.mav = common.MAVLink(None, srcSystem=1, srcComponent=1)
 
@@ -127,23 +137,32 @@ class MavProxy:
             await asyncio.sleep(1)
             dt = loop.time() - ts
             ts = loop.time()
-            radio_rssi = 127 + 128 + int(self.telem_chan.rssi.rssi)
-            logger.info("MAV UP: %s", up.bytes_cnt / dt)
-            logger.info("MAV DOWN: %s", down.bytes_cnt / dt)
-            logger.info("MAV RSSI: %s (%s)", self.telem_chan.rssi, radio_rssi)
+            radio_rssi = 127 + 128 + int(self.telem_chan.rssi)
+            logger.info("MAV UP: %s | DOWN %s | RSSI %s", int(up.bytes_cnt / dt), int(down.bytes_cnt / dt), self.telem_chan.rssi)
+            # logger.info("MAV DOWN: %s", )
+            # logger.info("MAV RSSI: %s (%s)", self.telem_chan.rssi, radio_rssi)
             up.bytes_cnt = 0
             down.bytes_cnt = 0
-
-            msg = common.MAVLink_radio_status_message(
-                rssi=radio_rssi,
-                remrssi=0,
-                txbuf=0,
-                noise=0,
-                remnoise=0,
-                rxerrors=0,
-                fixed=0)
-            data = msg.pack(mav)
-            gcs.sendto(data)
+            if self.mode == 'ground':
+                msg = common.MAVLink_radio_status_message(
+                    rssi=radio_rssi,
+                    remrssi=radio_rssi,
+                    txbuf=0,
+                    noise=0,
+                    remnoise=0,
+                    rxerrors=0,
+                    fixed=0)
+            else:
+                msg = common.MAVLink_radio_status_message(
+                    rssi=radio_rssi,
+                    remrssi=radio_rssi,
+                    txbuf=0,
+                    noise=0,
+                    remnoise=0,
+                    rxerrors=0,
+                    fixed=0)
+            data = msg.pack(self.mav)
+            gcs.sendto(data, ('127.0.0.1', 5557))
 
     async def start(self):
         logger.info("Starting MAV Proxy")
@@ -152,12 +171,17 @@ class MavProxy:
             lambda: UDPUplinkProtocol(),
             remote_addr=('127.0.0.1', 5557))
 
-        gs_link, gs_proto = await loop.create_datagram_endpoint(
-            lambda: GCSLinkProtocol(uplink),
-            remote_addr=('192.168.4.8', 14550), local_addr=('0.0.0.0', 5999))
+        if self.mode == 'ground':
+            gs_link, gs_proto = await loop.create_datagram_endpoint(
+                lambda: GCSLinkProtocol(uplink),
+                remote_addr=('192.168.4.8', 14550), local_addr=('0.0.0.0', 5999))
+        else:
+            gs_link, gs_proto = await loop.create_datagram_endpoint(
+                lambda: GCSLinkProtocol(uplink),
+                local_addr=('127.0.0.1', 14770))
 
         _, downlink_proto = await loop.create_datagram_endpoint(
-            lambda: UDPDownlinkProtocol(gs_link),
+            lambda: UDPDownlinkProtocol(gs_proto),
             local_addr=('127.0.0.1', 5556))
 
         asyncio.create_task(self.report(gs_proto, downlink_proto, gs_link))
@@ -239,8 +263,7 @@ class Channel:
             await asyncio.sleep(0.1)
             raw_data = await self.rx_proc.stdout.readline()
             raw_data = raw_data.decode()
-            logger.info("REPORT %s %s", self.name, raw_data)
-            #logger.info("STAT: %s", raw_data)
+            # logger.info("REPORT %s %s", self.name, raw_data)
             # 9638071\tANT\t0\t411:-75:-71:-68
             if 'ANT' in raw_data:
                 rssi_avg = raw_data.split(':')[-2]
@@ -261,14 +284,14 @@ async def main(args):
     ft_chan = Channel('ft', args.mode, args.iface, [3, 4], args.fec, 6557, 6556)
     telem_chan = Channel('telem', args.mode, args.iface, [5, 6], args.fec, 5557, 5556)
 
-    mav_proxy = MavProxy(telem_chan)
+    mav_proxy = MavProxy(args.mode, telem_chan)
 
     await bench_chan.start()
     await ft_chan.start()
     await telem_chan.start()
 
-    if args.mode == 'ground':
-        await mav_proxy.start()
+    # if args.mode == 'ground':
+    await mav_proxy.start()
 
     try:
         while True:
