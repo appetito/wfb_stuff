3574834b04ec60007afe0c1ee8833d0104278e70
Yuriy Homyakov <yuriy.homyakov@gmail.com>
2020-07-18 21:37:04 +0300

    reduce bench bitrate

--- bench_air.sh
+++ bench_air.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 /home/pi/wfb_stuff/wfb_bench.py --inport=7556 --outport=7557 --statport=5800 --packetsize=250 --sendpause=0.005 --mode=air
+python3 /home/pi/wfb_stuff/wfb_bench.py --inport=7556 --outport=7557 --statport=5800 --packetsize=1250 --sendpause=0.05 --mode=air
 
